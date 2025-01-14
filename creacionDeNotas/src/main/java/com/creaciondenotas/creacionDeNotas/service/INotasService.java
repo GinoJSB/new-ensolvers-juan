41908156e5e94587812ea9d67cabd1871fc68709
juan urrego <juan.u@hyros.com>
2025-01-13 22:48:45 -0500

    Update to return NoteResponseDTO instead of Note

--- creacionDeNotas/src/main/java/com/creaciondenotas/creacionDeNotas/service/INotasService.java
+++ creacionDeNotas/src/main/java/com/creaciondenotas/creacionDeNotas/service/INotasService.java
@@ -1,28 +1,27 @@
 package com.creaciondenotas.creacionDeNotas.service;
 
-import com.creaciondenotas.creacionDeNotas.model.DTO.NotaReqDTO;
+import com.creaciondenotas.creacionDeNotas.model.DTO.NoteReqDTO;
 import com.creaciondenotas.creacionDeNotas.model.DTO.NoteResponseDTO;
-import com.creaciondenotas.creacionDeNotas.model.Entity.Nota;
+import com.creaciondenotas.creacionDeNotas.model.Entity.Note;
 
 import java.util.List;
 
 public interface INotasService  {
 
     //metodo para crear una nota
-    public void saveNotas (Nota noti);
+    public void saveNotas (Note noti);
 
     //metodo para borrar una nota
     public void deleteNotas (Long id);
 
-    //metodo para encontrar una persona
-    public Nota findNotas  (Long id);
+    public NoteResponseDTO findNotas(Long id);
 
 
     //metodo para archivar nota
     public void archiveNotas (Long id, boolean archived);
 
 
-    NoteResponseDTO updateNote(Long id, NotaReqDTO notiReqDTO);
+    NoteResponseDTO updateNote(Long id, NoteReqDTO notiReqDTO);
 
-    List<Nota> getNotas();
+    List<NoteResponseDTO> getNotas();
 }
