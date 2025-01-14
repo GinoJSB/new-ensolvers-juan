1133a86aa971f70265ce12846be814fddf3ab550
juan urrego <juan.u@hyros.com>
2025-01-13 22:58:20 -0500

    Rename

--- creacionDeNotas/src/main/java/com/creaciondenotas/creacionDeNotas/repository/INotasRepository.java
+++ creacionDeNotas/src/main/java/com/creaciondenotas/creacionDeNotas/repository/INotasRepository.java
@@ -1,9 +1,9 @@
 package com.creaciondenotas.creacionDeNotas.repository;
 
-import com.creaciondenotas.creacionDeNotas.model.Entity.Nota;
+import com.creaciondenotas.creacionDeNotas.model.Entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface INotasRepository extends JpaRepository <Nota, Long>{
+public interface INotasRepository extends JpaRepository <Note, Long>{
 }
