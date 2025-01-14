15a20bbdf939a732361a4ce2d62f8bd2f6abaa65
juan urrego <juan.u@hyros.com>
2025-01-13 22:57:49 -0500

    Raname and return Response instead of base Note

--- creacionDeNotas/src/main/java/com/creaciondenotas/creacionDeNotas/service/NotasService.java
+++ creacionDeNotas/src/main/java/com/creaciondenotas/creacionDeNotas/service/NotasService.java
@@ -1,10 +1,10 @@
 package com.creaciondenotas.creacionDeNotas.service;
 
-import com.creaciondenotas.creacionDeNotas.model.DTO.NotaReqDTO;
+import com.creaciondenotas.creacionDeNotas.model.DTO.NoteReqDTO;
 import com.creaciondenotas.creacionDeNotas.model.DTO.NoteResponseDTO;
-import com.creaciondenotas.creacionDeNotas.model.Entity.Nota;
+import com.creaciondenotas.creacionDeNotas.model.Entity.Note;
+import com.creaciondenotas.creacionDeNotas.model.Entity.mapper.NoteMapper;
 import com.creaciondenotas.creacionDeNotas.repository.INotasRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -14,59 +14,70 @@ import java.util.Optional;
 @Service
 public class NotasService implements INotasService {
 
-    @Autowired
-    private INotasRepository notiRepo;
+    private final INotasRepository repository;
+    private final NoteMapper noteMapper;
 
-    @Override
-    public List<Nota> getNotas() {
-        return notiRepo.findAll();
+    public NotasService(INotasRepository repository, NoteMapper noteMapper) {
+        this.repository = repository;
+        this.noteMapper = noteMapper;
     }
 
     @Override
-    public void saveNotas(Nota noti) {
-        notiRepo.save(noti);
+    public List<NoteResponseDTO> getNotas() {
+        List<Note> notes = repository.findAll();
+
+        return noteMapper.notesToNotesResponseDTO(notes);
+    }
+
+    @Override
+    public void saveNotas(NoteReqDTO dto) {
+        // agarrar el array de categorias, tipo dto.getCategoryIds();
+        // va a armar una lista List<Category> categories = categoryService.findAllById(dto.getCategoryIds());
+        // cuando ya lo tenga, crea el objeto Note desde el NoteReqDTO.
+        // todo: Recuerde ignorar la property de categoryIds en el mapping.
+        // ya luego de que haga el mapper, sin las categorias, ahora si lo setea a mano las categorys que las obtuvo de categoryService.findAllById();
+
+        // y ahi guarda.
+        //repository.save(note);
     }
 
     @Override
     public void deleteNotas(Long id) {
-        notiRepo.deleteById(id);
+        repository.deleteById(id);
     }
 
     @Override
-    public Nota findNotas(Long id) {
-        return notiRepo.findById(id).orElse(null);
+    public NoteResponseDTO findNotas(Long id) {
+        Optional<Note> noteOpt = repository.findById(id);
+
+        return noteMapper.noteToNoteResponseDTO(getNoteIfPresent(noteOpt, id));
     }
 
     @Override
-    public NoteResponseDTO updateNote(Long id, NotaReqDTO notiReqDTO) {
-        Optional<Nota> notiOpt = notiRepo.findById(id);
+    public NoteResponseDTO updateNote(Long id, NoteReqDTO notiReqDTO) {
+        Optional<Note> noteOpt = repository.findById(id);
 
-        if (notiOpt.isEmpty()) {
-            throw new EntityNotFoundException("Nota con ID " + id + " no encontrada");
-        }
+        Note note = noteMapper.updateNoteReqDTOToNote(notiReqDTO, getNoteIfPresent(noteOpt, id));
+        repository.save(note);
 
-        Nota noti = notiOpt.get();
-        noti.setTitulo(notiReqDTO.getTitulo());
-        noti.setContenido(notiReqDTO.getContenido());
-        noti.setArchived(notiReqDTO.getArchived());
-
-        notiRepo.save(noti);
-
-        return NoteResponseDTO.builder()
-                .id(noti.getId())
-                .titulo(noti.getTitulo())
-                .contenido(noti.getContenido())
-                .archived(noti.isArchived())
-                .build();
+        return noteMapper.noteToNoteResponseDTO(note);
     }
-
 
     @Override
     public void archiveNotas(Long id, boolean archived) {
-        Nota noti = this.findNotas(id);
-        if (noti != null) {
-            noti.setArchived(archived);
-            this.saveNotas(noti);
+        Optional<Note> noteOpt = repository.findById(id);
+
+        Note note = getNoteIfPresent(noteOpt, id);
+
+        note.setArchived(archived);
+        repository.save(note);
+    }
+
+    private Note getNoteIfPresent(Optional<Note> note, Long id) {
+        if (note.isEmpty()) {
+            throw new EntityNotFoundException("Note with id: " + id + " not found");
         }
+
+        return note.get();
     }
 }
